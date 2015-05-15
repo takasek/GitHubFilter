3d5c64f41ac9e6398b7d4775049d3c04358c9e81
Yoshitaka Seki <takassekiyoshi@gmail.com>
2015-05-15 14:42:30 +0900

    Enable namespace

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 $(function(){
-	var issues = {
-		"開発" : "is:issue is:open -label:backlog -label:design",
-		"未着手" : "is:issue is:open -label:backlog -label:design -label:reviewing -label:checking -label:waiting-distribution",
-		"bug" : "is:issue is:open label:bug",
-	};
+	var conditions = {
+		"/10sec/iOS/issues" : {
+			"開発" : "is:issue is:open -label:backlog -label:design",
+			"未着手" : "is:issue is:open -label:backlog -label:design -label:reviewing -label:checking -label:waiting-distribution",
+			"bug" : "is:issue is:open label:bug",
+			"refactor" : "is:open label:refactoring",
+		}
+	}[document.location.pathname];
 	var generate = (function(title, query){
 		return $("<a>"+title+"</a>").addClass("js-selected-navigation-item subnav-item").attr("href", document.location.pathname+"?q="+encodeURIComponent(query).replace(/%20/g, "+"));
 	});
 	
-	for (title in issues) {
-		generate(title, issues[title]).appendTo($(".subnav-links"));
+	for (title in conditions) {
+		generate(title, conditions[title]).appendTo($(".subnav-links"));
 	}
 });
